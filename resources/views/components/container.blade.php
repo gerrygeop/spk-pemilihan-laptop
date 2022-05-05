a35b37ee70f98ae7dcf66a11ea2ed2bcc8a1a512
gerrygeop <gerryja28@gmail.com>
2022-05-05 23:32:33 +0800

    :cake: update UI component

---
+++ resources/views/components/container.blade.php
@@ -0,0 +1,5 @@
+<div class="py-12">
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/dapur/alternatif/index.blade.php
+++ resources/views/dapur/alternatif/index.blade.php
@@ -3,58 +3,56 @@
         Alternatif
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <x-container>
 
-            <div class="flex items-center mb-5">
-                <a href="{{ route('d.alternatif.create') }}" class="btn-primary">
-                    <i class='bx bxs-plus-circle bx-xs mr-1'></i>
-                    <span>Tambah Alternatif</span>
-                </a>
-            </div>
-
-            <x-table>
-                <thead class="bg-slate-50">
-                    <tr>
-                        <x-th>Kode</x-th>
-                        <x-th>Nama</x-th>
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Actions</span>
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody class="bg-white divide-y divide-slate-200">
-                    @forelse ($all_alternatif as $alternatif)
-                        <tr>
-                            <x-td>{{ $alternatif->kode }}</x-td>
-                            <x-td>{{ $alternatif->nama }}</x-td>
-
-                            <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                <div class="flex items-center justify-end">
-                                    <a href="{{ route('d.alternatif.show', $alternatif) }}" class="btn-hover-primary">Detail</a>
-                                    <a href="{{ route('d.alternatif.edit', $alternatif) }}" class="btn-hover-primary">Edit</a>
-
-                                    <form action="{{ route('d.alternatif.destroy', $alternatif) }}" method="POST" onsubmit="return confirm('Yakin untuk menghapus?')">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn-hover-danger">Hapus</button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-
-                    @empty
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap" colspan="4">
-                                <p class="text-sm text-center text-slate-500 italic">Belum ada data</p>
-                            </td>
-                        </tr>
-
-                    @endforelse
-                </tbody>
-            </x-table>
-                    
+        <div class="flex items-center mb-5">
+            <a href="{{ route('d.alternatif.create') }}" class="btn-primary">
+                <i class='bx bxs-plus-circle bx-xs mr-1'></i>
+                <span>Tambah Alternatif</span>
+            </a>
         </div>
-    </div>
+
+        <x-table>
+            <thead class="bg-slate-50">
+                <tr>
+                    <x-th>Kode</x-th>
+                    <x-th>Nama</x-th>
+                    <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Actions</span>
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody class="bg-white divide-y divide-slate-200">
+                @forelse ($all_alternatif as $alternatif)
+                    <tr>
+                        <x-td>{{ $alternatif->kode }}</x-td>
+                        <x-td>{{ $alternatif->nama }}</x-td>
+
+                        <td class="px-6 py-4 whitespace-nowrap text-sm">
+                            <div class="flex items-center justify-end">
+                                <a href="{{ route('d.alternatif.show', $alternatif) }}" class="btn-hover-primary">Detail</a>
+                                <a href="{{ route('d.alternatif.edit', $alternatif) }}" class="btn-hover-primary">Edit</a>
+
+                                <form action="{{ route('d.alternatif.destroy', $alternatif) }}" method="POST" onsubmit="return confirm('Yakin untuk menghapus?')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn-hover-danger">Hapus</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+
+                @empty
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap" colspan="4">
+                            <p class="text-sm text-center text-slate-500 italic">Belum ada data</p>
+                        </td>
+                    </tr>
+
+                @endforelse
+            </tbody>
+        </x-table>
+                    
+    </x-container>
 </x-app-layout>
